866d7f0ab490dfe6a67f837a10c4389d7525fef5
DannyDell <dwkaminsky@gmail.com>
2019-11-19 08:53:43 -0500

    Finished exam display/submission

--- main/scripts/student/displayExams.js
+++ main/scripts/student/displayExams.js
@@ -44,12 +44,73 @@ const renderExam = function(examID){
                 str += '<a class="panel-block is-active"><span class="panel-icon"><i class="fas fa-book" aria-hidden="true"></i></span>';
                 str+='D) '+inner_doc.data().D+'</a>';
                  $('#root').append(str);
+                 var select = document.createElement("SELECT");
+                 select.append(new Option("A", "A"));
+                 select.append(new Option("B", "B"));
+                 select.append(new Option("C", "C"));
+                 select.append(new Option("D", "D"));
+                 select.id = inner_doc.id;
+                 $('#root').append(select);
+                  break;
+                case 'tf':
+                  var root = document.getElementById("root");
+                  var select = document.createElement("SELECT");
+                  select.append(new Option("True", "T"));
+                  select.append(new Option("False", "F"));
+                  select.id = inner_doc.id;
+                  root.appendChild(select);
+                  break;
+                case 'sa':
+                  var root = document.getElementById("root");
+                  var textArea = document.createElement("TEXTAREA");
+                  textArea.id = inner_doc.id;
+                  root.appendChild(textArea);
                   break;
                 default:
+                  console.log("Error with Question");
                   break;
               }
         });
     });
+    setTimeout(function(){
+      var root = document.getElementById("root");
+      var submit = document.createElement("BUTTON");
+      submit.innerHTML = "Submit Exam";
+      $("#root").append("</br>");
+      root.appendChild(submit);
+      var tail = Math.floor(Math.random()*1000000000000000);
+      var ref =  db.collection("assessment_submissions").doc("Submission"+tail);
+      submit.onclick = function(){
+       ref.set({
+          STUDENT: firebase.auth().currentUser.uid,
+          ASSESSMENT: examID,
+      })
+      .then(function() {
+
+      })
+      .catch(function(error) {
+          console.error("Error writing document: ", error);
+      });
+        db.collection("questions").where("ASSESSMENT","==",examID).get().then(function(querySnapshot) {
+          querySnapshot.forEach(function(inner_doc) {
+            var answer = document.getElementById(inner_doc.id).value;
+            if(inner_doc.data().TYPE == 'mc'||inner_doc.data().TYPE=='tf'){
+              var q = inner_doc.id + "_score";
+              var MC_DATA = new Object();
+              MC_DATA[q] = Number(inner_doc.data().CORRECT == answer);
+              ref.set({MC_DATA}, { merge: true });
+              console.log(Number(inner_doc.data().CORRECT == answer));
+            }else{
+              var q = inner_doc.data().QUESTION;
+              var SA_DATA = new Object();
+              SA_DATA[q] = answer
+              ref.set({SA_DATA}, { merge: true });
+              console.log("Left to Grade: "+inner_doc.data().QUESTION);
+            }
+          });
+        });
+      }
+  }, 2000);
 });
 }
 
