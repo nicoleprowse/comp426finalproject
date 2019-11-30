8ce1741cb8c32109255a9b6a803b36230328c569
DannyDell <dwkaminsky@gmail.com>
2019-11-30 10:02:47 -0500

    Finished Create Exam

--- main/scripts/instructor/createExam.js
+++ main/scripts/instructor/createExam.js
@@ -9,6 +9,11 @@ firebase.initializeApp(firebaseConfig);
 var db = firebase.firestore();
 
 
+const submit_new_exam = function(number_questions){
+  console.log(number_questions)
+}
+
+
 const render_question_input = function(index){
   var root = document.getElementById("question-root");
   var container = document.createElement("DIV");
@@ -25,6 +30,14 @@ const render_question_input = function(index){
   type.append(new Option("True/False", "tf"));
   type.onchange = function(e){
     if(e.target.value == 'mc'){
+      try{
+        var div = document.getElementById("answers"+index);
+        container.removeChild(div);
+        var k = document.getElementById("correct"+index);
+        container.removeChild(k);
+        }catch(e){
+          console.log(e);
+        }
       var div = document.createElement("DIV");
       div.id = "answers"+index;
       var a = document.createElement("TEXTAREA")
@@ -47,9 +60,44 @@ const render_question_input = function(index){
       d.id = "d"+index;
       div.appendChild(d);
       container.appendChild(div);
-    }else{
+      var cont = document.createElement("DIV");
+      cont.id = 'cor'+index;
+      var correct = document.createElement("SELECT");
+      correct.id = "correct"+index;
+      correct.append(new Option("A", "a"));
+      correct.append(new Option("B", "b"));
+      correct.append(new Option("C", "c"));
+      correct.append(new Option("D", "d"));
+      cont.append('Correct: ');
+      cont.appendChild(correct);
+      container.appendChild(cont);
+    }else if(e.target.value=='tf'){
+      try{
       var div = document.getElementById("answers"+index);
       container.removeChild(div);
+      var k = document.getElementById("correct"+index);
+      container.removeChild(k);
+      }catch(e){
+        console.log(e);
+      }
+      var cont = document.createElement("DIV");
+      cont.id = 'cor'+index;
+      var correct = document.createElement("SELECT");
+      correct.id = "correct"+index;
+      correct.append(new Option("T", "t"));
+      correct.append(new Option("F", "f"));
+      cont.append('Correct: ');
+      cont.appendChild(correct);
+      container.appendChild(cont);
+    }else{
+      try{
+        var div = document.getElementById("answers"+index);
+        container.removeChild(div);
+        var k = document.getElementById("correct"+index);
+        container.removeChild(k);
+        }catch(e){
+          console.log(e);
+        }
     }
   };
   container.appendChild(type);
@@ -62,9 +110,13 @@ const register_user_listener = function(){
     if (user) {
         var info_root = document.getElementById("info-root");
         var examName = document.createElement("TEXTAREA");
-        examName.innerHTML = "Enter Exam Name Here"
+        var examTime = document.createElement("INPUT");
+        examTime.type = 'datetime-local';
+        examName.innerHTML = "Enter Exam Name Here";
         examName.id = "examName";
         info_root.appendChild(examName);
+        info_root.appendChild(document.createElement("BR"));
+        info_root.appendChild(examTime);
         info_root.appendChild(document.createElement("BR"));
         var courseSelect = document.createElement("SELECT");
         courseSelect.id = "course";
@@ -79,6 +131,47 @@ const register_user_listener = function(){
         var root = document.getElementById("button-root");
         var i = 1;
         render_question_input(0);
+        var submitButton = document.createElement("BUTTON");
+        submitButton.innerHTML = "Submit Exam";
+        submitButton.onclick = function(){
+          var tail = Math.floor(Math.random()*1000000000000000000);
+          var aName = 'Assessment'+tail
+          var examRef = db.collection("assessments").doc(aName);
+          examRef.set({
+            COURSE: courseSelect.value,
+            DATE: examTime.value,
+            NAME: examName.value,
+          });
+          for(var j = 0; j<i;j++){
+            var question = document.getElementById('q'+j).innerHTML;
+            var type = document.getElementById('t'+j).value;
+            var tail = Math.floor(Math.random()*1000000000000000000);
+            var questionRef = db.collection("questions").doc('Question'+tail);
+            questionRef.set({
+              ASSESSMENT: aName,
+              QUESTION: question,
+              TYPE: type,
+            }, { merge: true });
+            if(type!='sa'){
+              var correct = document.getElementById('correct'+j).value;
+              if(type=='mc'){
+                var a = document.getElementById('a'+j).value;
+                var b = document.getElementById('b'+j).value;
+                var c = document.getElementById('c'+j).value;
+                var d = document.getElementById('d'+j).value;
+                questionRef.set({
+                  A: a,
+                  B: b,
+                  C: c,
+                  D: d,
+                }, { merge: true });
+              }
+              questionRef.set({
+                CORRECT: correct,
+              }, { merge: true });
+            }
+          }
+        }
         var addQuestionButton = document.createElement("BUTTON");
         addQuestionButton.innerHTML = "Add Question";
         addQuestionButton.onclick = function(){
@@ -93,6 +186,7 @@ const register_user_listener = function(){
           q_root.removeChild(document.getElementById("container"+i));
         }
         root.appendChild(deleteQuestionButton);
+        root.appendChild(submitButton);
     } else {
       console.log("Successful Sign Out!");
     }
